marble-development/android_device_xiaomi_marble_legacy

marble: Partially import parts from raphael & sm8350-common
Updates to:

* Dirac
* Doze

Added:

* Thermals
* Per-app refresh rate
* Clear speaker

Co-authored-by: Bruno Martins <bgcngm@gmail.com>
Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
Co-authored-by: nullxception <enuma.alrizky@gmail.com>
Co-authored-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>
Co-authored-by: TheScarastic <warabhishek@gmail.com>

parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -1,0 +1,43 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017-2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.refreshrate.RefreshUtils;
+import org.lineageos.settings.thermal.ThermalUtils;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "XiaomiParts";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        DiracUtils.initialize(context);
+        DozeUtils.checkDozeService(context);
+        RefreshUtils.initialize(context);
+        ThermalUtils.startService(context);
+    }
+}

parts/src/org/lineageos/settings/speaker/ClearSpeakerFragment.java
@@ -1,0 +1,121 @@
+/*
+ * Copyright (C) 2020 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.speaker;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import org.lineageos.settings.R;
+
+import java.io.IOException;
+
+public class ClearSpeakerFragment extends PreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = ClearSpeakerFragment.class.getSimpleName();
+
+    private static final String PREF_CLEAR_SPEAKER = "clear_speaker_pref";
+
+    private AudioManager mAudioManager;
+    private Handler mHandler;
+    private MediaPlayer mMediaPlayer;
+    private SwitchPreference mClearSpeakerPref;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.clear_speaker_settings);
+
+        mClearSpeakerPref = (SwitchPreference) findPreference(PREF_CLEAR_SPEAKER);
+        mClearSpeakerPref.setOnPreferenceChangeListener(this);
+
+        mHandler = new Handler();
+        mAudioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mClearSpeakerPref) {
+            boolean value = (Boolean) newValue;
+            if (value) {
+                if (startPlaying()) {
+                    mHandler.removeCallbacksAndMessages(null);
+                    mHandler.postDelayed(() -> {
+                        stopPlaying();
+                    }, 30000);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        stopPlaying();
+    }
+
+    public boolean startPlaying() {
+        mAudioManager.setParameters("status_earpiece_clean=on");
+        mMediaPlayer = new MediaPlayer();
+        getActivity().setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setLooping(true);
+        try {
+            AssetFileDescriptor file = getResources().openRawResourceFd(R.raw.clear_speaker_sound);
+            try {
+                mMediaPlayer.setDataSource(file.getFileDescriptor(), file.getStartOffset(), file.getLength());
+            } finally {
+                file.close();
+            }
+            mClearSpeakerPref.setEnabled(false);
+            mMediaPlayer.setVolume(1.0f, 1.0f);
+            mMediaPlayer.prepare();
+            mMediaPlayer.start();
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to play speaker clean sound!", ioe);
+            return false;
+        }
+        return true;
+    }
+
+    public void stopPlaying() {
+        if (mMediaPlayer != null) {
+            if (mMediaPlayer.isPlaying()) {
+                mMediaPlayer.stop();
+                mMediaPlayer.reset();
+                mMediaPlayer.release();
+                mMediaPlayer=null;
+            }
+        }
+        mAudioManager.setParameters("status_earpiece_clean=off");
+        mClearSpeakerPref.setEnabled(true);
+        mClearSpeakerPref.setChecked(false);
+    }
+}

parts/src/org/lineageos/settings/thermal/ThermalActivity.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2020,2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.thermal;
+
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class ThermalActivity extends CollapsingToolbarBaseActivity {
+    private static final String TAG_THERMAL = "thermal";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+         getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new ThermalSettingsFragment(), TAG_THERMAL).commit();
+    }
+}

parts/src/org/lineageos/settings/thermal/ThermalUtils.java
@@ -1,0 +1,161 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.thermal;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.settings.utils.FileUtils;
+
+public final class ThermalUtils {
+
+    private static final String THERMAL_CONTROL = "thermal_control";
+
+    protected static final int STATE_DEFAULT = 0;
+    protected static final int STATE_BENCHMARK = 1;
+    protected static final int STATE_BROWSER = 2;
+    protected static final int STATE_CAMERA = 3;
+    protected static final int STATE_DIALER = 4;
+    protected static final int STATE_GAMING = 5;
+    protected static final int STATE_STREAMING = 6;
+
+    private static final String THERMAL_STATE_DEFAULT = "0";
+    private static final String THERMAL_STATE_BENCHMARK = "10";
+    private static final String THERMAL_STATE_BROWSER = "11";
+    private static final String THERMAL_STATE_CAMERA = "12";
+    private static final String THERMAL_STATE_DIALER = "8";
+    private static final String THERMAL_STATE_GAMING = "9";
+    private static final String THERMAL_STATE_STREAMING = "14";
+
+    private static final String THERMAL_BENCHMARK = "thermal.benchmark=";
+    private static final String THERMAL_BROWSER = "thermal.browser=";
+    private static final String THERMAL_CAMERA = "thermal.camera=";
+    private static final String THERMAL_DIALER = "thermal.dialer=";
+    private static final String THERMAL_GAMING = "thermal.gaming=";
+    private static final String THERMAL_STREAMING = "thermal.streaming=";
+
+    private static final String THERMAL_SCONFIG = "/sys/class/thermal/thermal_message/sconfig";
+
+    private SharedPreferences mSharedPrefs;
+
+    protected ThermalUtils(Context context) {
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static void startService(Context context) {
+        context.startServiceAsUser(new Intent(context, ThermalService.class),
+                UserHandle.CURRENT);
+    }
+
+    private void writeValue(String profiles) {
+        mSharedPrefs.edit().putString(THERMAL_CONTROL, profiles).apply();
+    }
+
+    private String getValue() {
+        String value = mSharedPrefs.getString(THERMAL_CONTROL, null);
+
+        if (value == null || value.isEmpty()) {
+            value = THERMAL_BENCHMARK + ":" + THERMAL_BROWSER + ":" + THERMAL_CAMERA + ":" +
+                    THERMAL_DIALER + ":" + THERMAL_GAMING + ":" + THERMAL_STREAMING;
+            writeValue(value);
+        }
+        return value;
+    }
+
+    protected void writePackage(String packageName, int mode) {
+        String value = getValue();
+        value = value.replace(packageName + ",", "");
+        String[] modes = value.split(":");
+        String finalString;
+
+        switch (mode) {
+            case STATE_BENCHMARK:
+                modes[0] = modes[0] + packageName + ",";
+                break;
+            case STATE_BROWSER:
+                modes[1] = modes[1] + packageName + ",";
+                break;
+            case STATE_CAMERA:
+                modes[2] = modes[2] + packageName + ",";
+                break;
+            case STATE_DIALER:
+                modes[3] = modes[3] + packageName + ",";
+                break;
+            case STATE_GAMING:
+                modes[4] = modes[4] + packageName + ",";
+                break;
+            case STATE_STREAMING:
+                modes[5] = modes[5] + packageName + ",";
+                break;
+        }
+
+        finalString = modes[0] + ":" + modes[1] + ":" + modes[2] + ":" + modes[3] + ":" +
+                modes[4] + ":" + modes[5];
+
+        writeValue(finalString);
+    }
+
+    protected int getStateForPackage(String packageName) {
+        String value = getValue();
+        String[] modes = value.split(":");
+        int state = STATE_DEFAULT;
+        if (modes[0].contains(packageName + ",")) {
+            state = STATE_BENCHMARK;
+        } else if (modes[1].contains(packageName + ",")) {
+            state = STATE_BROWSER;
+        } else if (modes[2].contains(packageName + ",")) {
+            state = STATE_CAMERA;
+        } else if (modes[3].contains(packageName + ",")) {
+            state = STATE_DIALER;
+        } else if (modes[4].contains(packageName + ",")) {
+            state = STATE_GAMING;
+        } else if (modes[5].contains(packageName + ",")) {
+            state = STATE_STREAMING;
+        }
+
+        return state;
+    }
+
+    protected void setThermalProfile(String packageName) {
+        String value = getValue();
+        String modes[];
+        String state = THERMAL_STATE_DEFAULT;
+
+        if (value != null) {
+            modes = value.split(":");
+
+            if (modes[0].contains(packageName + ",")) {
+                state = THERMAL_STATE_BENCHMARK;
+            } else if (modes[1].contains(packageName + ",")) {
+                state = THERMAL_STATE_BROWSER;
+            } else if (modes[2].contains(packageName + ",")) {
+                state = THERMAL_STATE_CAMERA;
+            } else if (modes[3].contains(packageName + ",")) {
+                state = THERMAL_STATE_DIALER;
+            } else if (modes[4].contains(packageName + ",")) {
+                state = THERMAL_STATE_GAMING;
+            } else if (modes[5].contains(packageName + ",")) {
+                state = THERMAL_STATE_STREAMING;
+            }
+        }
+        FileUtils.writeLine(THERMAL_SCONFIG, state);
+    }
+}

crdroidandroid/android_device_xiaomi_marble

parts/src/org/pixelexperience/settings/BootCompletedReceiver.java
@@ -1,0 +1,43 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017-2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.pixelexperience.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import org.pixelexperience.settings.dirac.DiracUtils;
+import org.pixelexperience.settings.doze.DozeUtils;
+import org.pixelexperience.settings.refreshrate.RefreshUtils;
+import org.pixelexperience.settings.thermal.ThermalUtils;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "XiaomiParts";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        DiracUtils.initialize(context);
+        DozeUtils.checkDozeService(context);
+        RefreshUtils.initialize(context);
+        ThermalUtils.startService(context);
+    }
+}

parts/src/org/pixelexperience/settings/speaker/ClearSpeakerFragment.java
@@ -1,0 +1,121 @@
+/*
+ * Copyright (C) 2020 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.pixelexperience.settings.speaker;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import org.pixelexperience.settings.R;
+
+import java.io.IOException;
+
+public class ClearSpeakerFragment extends PreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = ClearSpeakerFragment.class.getSimpleName();
+
+    private static final String PREF_CLEAR_SPEAKER = "clear_speaker_pref";
+
+    private AudioManager mAudioManager;
+    private Handler mHandler;
+    private MediaPlayer mMediaPlayer;
+    private SwitchPreference mClearSpeakerPref;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.clear_speaker_settings);
+
+        mClearSpeakerPref = (SwitchPreference) findPreference(PREF_CLEAR_SPEAKER);
+        mClearSpeakerPref.setOnPreferenceChangeListener(this);
+
+        mHandler = new Handler();
+        mAudioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mClearSpeakerPref) {
+            boolean value = (Boolean) newValue;
+            if (value) {
+                if (startPlaying()) {
+                    mHandler.removeCallbacksAndMessages(null);
+                    mHandler.postDelayed(() -> {
+                        stopPlaying();
+                    }, 30000);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        stopPlaying();
+    }
+
+    public boolean startPlaying() {
+        mAudioManager.setParameters("status_earpiece_clean=on");
+        mMediaPlayer = new MediaPlayer();
+        getActivity().setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setLooping(true);
+        try {
+            AssetFileDescriptor file = getResources().openRawResourceFd(R.raw.clear_speaker_sound);
+            try {
+                mMediaPlayer.setDataSource(file.getFileDescriptor(), file.getStartOffset(), file.getLength());
+            } finally {
+                file.close();
+            }
+            mClearSpeakerPref.setEnabled(false);
+            mMediaPlayer.setVolume(1.0f, 1.0f);
+            mMediaPlayer.prepare();
+            mMediaPlayer.start();
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to play speaker clean sound!", ioe);
+            return false;
+        }
+        return true;
+    }
+
+    public void stopPlaying() {
+        if (mMediaPlayer != null) {
+            if (mMediaPlayer.isPlaying()) {
+                mMediaPlayer.stop();
+                mMediaPlayer.reset();
+                mMediaPlayer.release();
+                mMediaPlayer=null;
+            }
+        }
+        mAudioManager.setParameters("status_earpiece_clean=off");
+        mClearSpeakerPref.setEnabled(true);
+        mClearSpeakerPref.setChecked(false);
+    }
+}

parts/src/org/pixelexperience/settings/thermal/ThermalActivity.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2020,2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.pixelexperience.settings.thermal;
+
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class ThermalActivity extends CollapsingToolbarBaseActivity {
+    private static final String TAG_THERMAL = "thermal";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+         getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new ThermalSettingsFragment(), TAG_THERMAL).commit();
+    }
+}

parts/src/org/pixelexperience/settings/thermal/ThermalUtils.java
@@ -1,0 +1,161 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.pixelexperience.settings.thermal;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+
+import androidx.preference.PreferenceManager;
+
+import org.pixelexperience.settings.utils.FileUtils;
+
+public final class ThermalUtils {
+
+    private static final String THERMAL_CONTROL = "thermal_control";
+
+    protected static final int STATE_DEFAULT = 0;
+    protected static final int STATE_BENCHMARK = 1;
+    protected static final int STATE_BROWSER = 2;
+    protected static final int STATE_CAMERA = 3;
+    protected static final int STATE_DIALER = 4;
+    protected static final int STATE_GAMING = 5;
+    protected static final int STATE_STREAMING = 6;
+
+    private static final String THERMAL_STATE_DEFAULT = "0";
+    private static final String THERMAL_STATE_BENCHMARK = "10";
+    private static final String THERMAL_STATE_BROWSER = "11";
+    private static final String THERMAL_STATE_CAMERA = "12";
+    private static final String THERMAL_STATE_DIALER = "8";
+    private static final String THERMAL_STATE_GAMING = "9";
+    private static final String THERMAL_STATE_STREAMING = "14";
+
+    private static final String THERMAL_BENCHMARK = "thermal.benchmark=";
+    private static final String THERMAL_BROWSER = "thermal.browser=";
+    private static final String THERMAL_CAMERA = "thermal.camera=";
+    private static final String THERMAL_DIALER = "thermal.dialer=";
+    private static final String THERMAL_GAMING = "thermal.gaming=";
+    private static final String THERMAL_STREAMING = "thermal.streaming=";
+
+    private static final String THERMAL_SCONFIG = "/sys/class/thermal/thermal_message/sconfig";
+
+    private SharedPreferences mSharedPrefs;
+
+    protected ThermalUtils(Context context) {
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static void startService(Context context) {
+        context.startServiceAsUser(new Intent(context, ThermalService.class),
+                UserHandle.CURRENT);
+    }
+
+    private void writeValue(String profiles) {
+        mSharedPrefs.edit().putString(THERMAL_CONTROL, profiles).apply();
+    }
+
+    private String getValue() {
+        String value = mSharedPrefs.getString(THERMAL_CONTROL, null);
+
+        if (value == null || value.isEmpty()) {
+            value = THERMAL_BENCHMARK + ":" + THERMAL_BROWSER + ":" + THERMAL_CAMERA + ":" +
+                    THERMAL_DIALER + ":" + THERMAL_GAMING + ":" + THERMAL_STREAMING;
+            writeValue(value);
+        }
+        return value;
+    }
+
+    protected void writePackage(String packageName, int mode) {
+        String value = getValue();
+        value = value.replace(packageName + ",", "");
+        String[] modes = value.split(":");
+        String finalString;
+
+        switch (mode) {
+            case STATE_BENCHMARK:
+                modes[0] = modes[0] + packageName + ",";
+                break;
+            case STATE_BROWSER:
+                modes[1] = modes[1] + packageName + ",";
+                break;
+            case STATE_CAMERA:
+                modes[2] = modes[2] + packageName + ",";
+                break;
+            case STATE_DIALER:
+                modes[3] = modes[3] + packageName + ",";
+                break;
+            case STATE_GAMING:
+                modes[4] = modes[4] + packageName + ",";
+                break;
+            case STATE_STREAMING:
+                modes[5] = modes[5] + packageName + ",";
+                break;
+        }
+
+        finalString = modes[0] + ":" + modes[1] + ":" + modes[2] + ":" + modes[3] + ":" +
+                modes[4] + ":" + modes[5];
+
+        writeValue(finalString);
+    }
+
+    protected int getStateForPackage(String packageName) {
+        String value = getValue();
+        String[] modes = value.split(":");
+        int state = STATE_DEFAULT;
+        if (modes[0].contains(packageName + ",")) {
+            state = STATE_BENCHMARK;
+        } else if (modes[1].contains(packageName + ",")) {
+            state = STATE_BROWSER;
+        } else if (modes[2].contains(packageName + ",")) {
+            state = STATE_CAMERA;
+        } else if (modes[3].contains(packageName + ",")) {
+            state = STATE_DIALER;
+        } else if (modes[4].contains(packageName + ",")) {
+            state = STATE_GAMING;
+        } else if (modes[5].contains(packageName + ",")) {
+            state = STATE_STREAMING;
+        }
+
+        return state;
+    }
+
+    protected void setThermalProfile(String packageName) {
+        String value = getValue();
+        String modes[];
+        String state = THERMAL_STATE_DEFAULT;
+
+        if (value != null) {
+            modes = value.split(":");
+
+            if (modes[0].contains(packageName + ",")) {
+                state = THERMAL_STATE_BENCHMARK;
+            } else if (modes[1].contains(packageName + ",")) {
+                state = THERMAL_STATE_BROWSER;
+            } else if (modes[2].contains(packageName + ",")) {
+                state = THERMAL_STATE_CAMERA;
+            } else if (modes[3].contains(packageName + ",")) {
+                state = THERMAL_STATE_DIALER;
+            } else if (modes[4].contains(packageName + ",")) {
+                state = THERMAL_STATE_GAMING;
+            } else if (modes[5].contains(packageName + ",")) {
+                state = THERMAL_STATE_STREAMING;
+            }
+        }
+        FileUtils.writeLine(THERMAL_SCONFIG, state);
+    }
+}