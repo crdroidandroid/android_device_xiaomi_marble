f873761bbb3167a3ff9e4235fd29949f0ab97d9e
AdarshGrewal <adarshgrewal@gmail.com>
2024-10-16 00:29:49 +0530

    marble: Include MiuiCamera if exists
    Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,10 +10,16 @@ include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 # Inherit from the proprietary version
 include vendor/xiaomi/marble/BoardConfigVendor.mk
 
+# Inherit from proprietary files for miuicamera
+-include device/xiaomi/miuicamera-marble/BoardConfig.mk
+
 DEVICE_PATH := device/xiaomi/marble
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := marble,marblein
+
+# Camera - Miui
+TARGET_CAMERA_PACKAGE_NAME := com.android.camera
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
