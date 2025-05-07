a1307e69fb17e06c1e448d7793481aad9f7db9ea
chaitanya <chaitanyaarora3@gmail.com>
2025-05-08 04:27:06 +0530

    marble: Stop including firmware

--- extract-files.py
+++ extract-files.py
@@ -65,7 +65,7 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
     check_elf=True,
-    add_firmware_proprietary_file=True,
+    add_firmware_proprietary_file=False,
 )
 
 if __name__ == '__main__':
