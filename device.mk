6c6b932855ba63ddb3f61a95d1931149240843fe
Michael Bestas <mkbestas@lineageos.org>
2026-01-04 21:00:31 +0530

    marble: Update vibrator soong config variable to bool type

--- device.mk
+++ device.mk
@@ -81,4 +81,4 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 $(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.xiaomi)
-$(call soong_config_set,qti_vibrator,use_effect_stream,true)
+$(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
